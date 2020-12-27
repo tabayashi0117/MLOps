cb0c4158e6bfa91a4b6197ec03ce9bcbb733db2e
tabayashi <tabayashi@ubuntu20.04>
2020-12-27 19:22:20 +0900

    add main, test_main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def testfunc(a):
+    print(a)
+    return a+1
+
+if __name__ == "__main__":
+    testfunc(1)
+    testfunc(2)
---
+++ test_main.py
@@ -0,0 +1,7 @@
+from main import testfunc
+
+def test1():
+    assert testfunc(1) == 2
+
+def test2():
+    assert testfunc(2) == 3
